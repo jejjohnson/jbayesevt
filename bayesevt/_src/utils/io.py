ae1467b6723c3d77a41313d490590200e1fc92af
jejjohnson <emanjohnson91@gmail.com>
2024-03-04 16:16:20 +0100

    New Demo for ai4storylines.

---
+++ bayesevt/_src/utils/io.py
@@ -0,0 +1,8 @@
+import glob, os
+
+
+def get_list_filenames(data_path: str="./", ext: str="*"):
+    """Loads a list of file names within a directory
+    """
+    pattern = f"*{ext}"
+    return sorted(glob.glob(os.path.join(data_path, "**", pattern), recursive=True))
