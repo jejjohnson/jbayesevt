d96db4478b5f8939b1f915265832e3f947a5dc18
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2024-03-21 14:40:15 +0100

    More automated EVTs.

--- bayesevt/_src/geoprocessing/math.py
+++ bayesevt/_src/geoprocessing/math.py
@@ -3,7 +3,16 @@ import numpy as np
 
 # SECOND AXIS
 def safe_reciprocal(x):
-    """Vectorized 1/x, treating x==0 manually"""
+    """
+    Vectorized 1/x, treating x==0 manually.
+
+    Parameters:
+    - x: Input array or scalar.
+
+    Returns:
+    - Array or scalar with reciprocal values of x. If x is 0, returns infinity.
+
+    """
     x = np.array(x, float)
     near_zero = np.isclose(x, 0)
     x[near_zero] = np.inf
